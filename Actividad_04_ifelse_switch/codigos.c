7cf2f594ffa4eed411bdc01c83f72073b85aeb6c
renataastorga2404-pixel <renata.astorga2404@alumnos.udg.mx>
2026-05-23 15:21:35 -0600

    Refactor code to use switch for menu options

--- Actividad_04_ifelse_switch/codigos.c
+++ Actividad_04_ifelse_switch/codigos.c
@@ -28,12 +28,11 @@ printf("Tu numero es impar\n");}
 return 0;
 }
 
--PRIMOS switch
+-PRIMOS if
 #include<stdio.h>
 int main (){
 int numero;
 int i=2;
-int primo=1;
 
 printf("Ingresa un numero:\n");
 scanf("%d",&numero);
@@ -74,3 +73,89 @@ else{
 }
 return 0;
 }
+
+-Calculadora Switch
+#include <stdio.h>
+int main()
+{
+  int opcionP;
+  int opcionC;
+  int x;
+  int y;
+
+do{
+  printf("------Bienvenido al menu-------\n");
+  printf("Elige una opcion:\n");
+  printf("...1.Analizar un numero:\n");
+  printf("...2.Calculadora:\n");
+  printf("...3.Salir:\n");
+  scanf("%d",&opcionP);
+
+  switch(opcionP)
+  {
+    case 1://Analizar el numero
+        printf("...Opcion seleccionador: Analizar un numero...\n");
+        printf("Ingresa un numero entero:\n");
+        scanf("%d",&x);
+
+        if(x %2 == 0){
+            printf("Tu numero es par\n\n");
+        }else{
+            printf("Tu numero es impar\n\n");
+            }
+
+        break;
+
+    case 2://Calculadora
+         printf("\nOpcion seleccionado calculadora\n");
+
+         printf("Elige una operacion que quisiera realizar\n");
+         printf("...1.Sumar...\n");
+         printf("...2.Restar...\n");
+         printf("...3.Regresar...\n");
+         scanf("%d",&opcionC);
+
+         switch(opcionC)
+         {
+        case 1://Sumar
+            printf("Ingresa el primer numero:");
+            scanf("%d",&x);
+            printf("Ingresa el segundo numero:");
+            scanf("%d",&y);
+
+            printf("El resultado de tu suma es %d\n\n", x+y);
+            break;
+
+        case 2://restar
+            printf("Ingresa el primer numero:");
+            scanf("%d",&x);
+            printf("Ingresa el segundo numero:");
+            scanf("%d",&y);
+
+            printf("El resultado de tu resta es %d\n\n", x-y);
+            break;
+
+        case 3://regresar
+            printf("...Termino la calculadora...\n\n");
+            break;
+
+             default:
+            printf("Opcion No valida, Ingresa otra opcion:\n");
+         }
+
+        break;
+
+    case 3://salir
+        printf("----Fin del programa----\n");
+        return 0;
+        break;
+
+
+    default:
+     printf("-----Opcion NO valida------\n");
+
+  }
+}while(opcionP !=3);
+return 0;
+}
+
