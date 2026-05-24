40c08a5f3296aae2ddafeaa0aba0cc16bd5e9996
renataastorga2404-pixel <renata.astorga2404@alumnos.udg.mx>
2026-05-23 20:12:06 -0600

    Add functions to calculate double and triple of a number

---
+++ Actividad_10_PunterosyMemoria/codigo.c
@@ -0,0 +1,23 @@
+DOBLE Y TRIPLE
+#include <stdio.h>
+#include <stdlib.h>
+int f_doble (int a){
+
+return a*2;}
+
+void f_triple(int *a){
+ (*a) = (*a) * 3;
+}
+int main()
+{   int a;
+    printf("Dame un numero del 1 al 100:\n");
+    scanf("%d",&a);
+
+    f_doble(a);
+    printf("doble: %d\n",f_doble(a));
+
+    f_triple(&a);
+    printf("triple: %d\n",a);
+    return 0;
+}
+
